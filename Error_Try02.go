7df85c39238d89f7e8ce7e4dc247e42d39b8b6af
Frotten <frottenice@outlook.com>
2025-05-26 22:41:43 +0800

    Error_Try_Test

---
+++ Error_Try02.go
@@ -0,0 +1,11 @@
+package main
+
+//func main() {
+//	defer func() {
+//		if Re := recover(); Re != nil {
+//			fmt.Println("抵达修复程序")
+//			fmt.Println(Re)
+//		}
+//	}()
+//	panic("真正的幸福从何而寻？")
+//}
