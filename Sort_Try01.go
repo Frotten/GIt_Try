83142bcf4ca48af4875d331a3d68dccbec98be2a
Frotten <frottenice@outlook.com>
2025-05-28 18:38:20 +0800

    Rename

--- Sort_Try01.go
+++ Sort_Try01.go
@@ -5,26 +5,26 @@ import (
 	"sort"
 )
 
-type Person struct {
+type PersonSort struct {
 	Name string
 	Age  int
 }
 
-type Byage []Person
+type Byage []PersonSort
 
 func (a Byage) Len() int           { return len(a) }
 func (a Byage) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a Byage) Less(i, j int) bool { return a[i].Age < a[j].Age }
 
 func main() {
-	peopel := []Person{
+	people := []PersonSort{
 		{"Alex", 66},
 		{"Steve", 55},
 		{"Him", 404},
 	}
-	sort.Sort(Byage(peopel))
-	fmt.Println("By name:", peopel)
-	people := []Person{
+	sort.Sort(Byage(people))
+	fmt.Println("By name:", people)
+	people = []PersonSort{
 		{"Alex", 18},
 		{"Blame", 16},
 		{"Nell", 19},
