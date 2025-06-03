92388f4c8ef19d4eed2bf63b69a259c9f7b9efb7
Frotten <frottenice@outlook.com>
2025-06-03 22:15:03 +0800

    New Add

---
+++ Reflect_Try06.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	A := "A"
+	fmt.Println("TypeOf(A):", reflect.TypeOf(A), " Kind of A :", reflect.TypeOf(A).Kind())
+	B := 'B'
+	fmt.Println("TypeOf(B):", reflect.TypeOf(B), " Kind of B :", reflect.TypeOf(B).Kind())
+	var C rune = 'C'
+	fmt.Println("TypeOf(C):", reflect.TypeOf(C), " Kind of C :", reflect.TypeOf(C).Kind())
+}
