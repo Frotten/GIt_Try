50ec49ef2ae6ea99100f319b28fbdb5de914e15f
Frotten <frottenice@outlook.com>
2025-06-03 22:20:23 +0800

    Update

--- Reflect_Try06.go
+++ Reflect_Try06.go
@@ -12,4 +12,10 @@ func main() {
 	fmt.Println("TypeOf(B):", reflect.TypeOf(B), " Kind of B :", reflect.TypeOf(B).Kind())
 	var C rune = 'C'
 	fmt.Println("TypeOf(C):", reflect.TypeOf(C), " Kind of C :", reflect.TypeOf(C).Kind())
+	VC := reflect.ValueOf(C).Interface()
+	if VVC, ok := VC.(int32); ok {
+		fmt.Println(int(VVC), string(VVC))
+	} else {
+		fmt.Println(ok)
+	}
 }
