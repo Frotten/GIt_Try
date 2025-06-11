b39e348a66cbc886dab1289ceeceba8c723e9118
Frotten <frottenice@outlook.com>
2025-06-11 19:25:21 +0800

    Note

--- Gin_Try02.go
+++ Gin_Try02.go
@@ -8,7 +8,8 @@ import (
 func main() {
 	engine := gin.Default()
 	engine.GET("/web", func(c *gin.Context) {
-		getname := c.Query("name") //这里的key为name，因此我们在对应网址后加上对应的key和value即可传入，如xxxx/web?name=AAAA
+		//getname := c.Query("name") //这里的key为name，因此我们在对应网址后加上对应的key和value即可传入，如xxxx/web?name=AAAA
+		getname := c.DefaultQuery("name", "no thing to search")
 		c.JSON(http.StatusOK, gin.H{
 			"name": getname, //这里是json的输出格式，如果按照上面的web?name=AAAA的话，这里也就是按照"name": AAAA的模板来进行的
 		})
